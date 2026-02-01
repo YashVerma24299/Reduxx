504650102e8ebb1efacda972af1b04b7fce0173f
Yash <ritikverma23@gmail.com>
2026-02-01 18:37:59 +0530

    Update the basic redux code

--- 2_Redux/Basic/Practice.js
+++ 2_Redux/Basic/Practice.js
@@ -6,24 +6,28 @@ let initialState = {
     name: 'Yash',
     cnt: 0
 }
+const increment = () => ({ type: "post/increament" });
+const decrement = () => ({ type: "post/decreament" });
+const increaseBy =(value)=>({ type: "post/increamentBy", payload: value})
+const decreaseBy =(value)=>({ type: "post/decreamentBy", payload: value})
 
 function reducer(state=initialState, action) {
-    if(action.type === 'post/increamentBy')
+    if(action.type === increaseBy)
         return { ...state, cnt: state.cnt + action.payload }
-    else if(action.type === 'post/decreamentBy')
+    else if(action.type === decreaseBy)
         return { ...state, cnt: state.cnt - action.payload }
-    else if(action.type === 'post/increament')
+    else if(action.type === increment)
         return { ...state, cnt: state.cnt + 1 }
-    else if(action.type === 'post/decreament')
+    else if(action.type === decrement)
         return { ...state, cnt: state.cnt - 1 }
     else    
         return state;
 }
-initialState = reducer(initialState, {type: 'post/increament'})
+initialState = reducer(initialState, {type: increment})
 console.log(initialState)
-initialState = reducer(initialState, {type: 'post/increamentBy', payload: 10})
+initialState = reducer(initialState, {type: increaseBy, payload: 10})
 console.log(initialState)
-initialState = reducer(initialState, {type: 'post/decreamentBy', payload: 10} )
+initialState = reducer(initialState, {type: decreaseBy, payload: 10} )
 console.log(initialState)
 initialState = reducer(initialState, {type: 'post/yash'})
 console.log(initialState)
@@ -32,7 +36,7 @@ console.log(initialState)
 
 const store = createStore(reducer)
 console.log(store)
-// In reducer function, if we assign a value to the state that can be shown by getState
+// In reducer function(line number 10), if we assign a value to the state that can be shown by getState
 console.log(store.getState())
 
 
@@ -40,7 +44,7 @@ console.log(store.getState())
 store.subscribe(() => {
     console.log('STATE 👉', store.getState());
 });
-store.dispatch({type:"post/increament"})
+store.dispatch({type:increment})
 // console.log(store.getState())
-store.dispatch({type:"post/increamentBy", payload: 10})
+store.dispatch({type:increaseBy, payload: 10})
 // console.log(store.getState())  
